55b5c75c43d5dbec12b26d7f21f3911a5a79f78a
danny <pss>
2018-10-14 16:16:39 +0900

    test js code\n - list \n - activity \n - card

---
+++ test/activity.spec.js
@@ -0,0 +1,10 @@
+const assert = require("assert");
+const sut = require("../service/activity");
+
+// 보드에 종속
+
+describe("카드", () => {
+    describe("보드가 있어야 존재가능", () => {
+
+    });
+});
---
+++ test/card.spec.js
@@ -0,0 +1,8 @@
+const assert = require("assert");
+const sut = require("../service/card");
+
+// 리스트가 존재해야 카드가 존재할 수 있음
+
+describe("카드", () => {
+    
+});
