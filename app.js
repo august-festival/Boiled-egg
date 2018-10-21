c8b36641e869bf8c07363eddf96a9afd6f67dd7d
super-fishz <super_fishz@core-a.org>
2018-10-21 16:45:05 +0900

    route 펑션을 테스트 할 수 있는 샘플 추가
    `npm run test:api:board` 하면 테스트 해볼 수 있음

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const favicon = require("serve-favicon");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
-const passportConfig = require('./passport');
 const bodyParser = require("body-parser");
+const passportConfig = require("./passport");
 
 const index = require("./routes/index");
 const users = require("./routes/users");
+const boards = require("./routes/api/v1/board");
 
 const app = express();
 
@@ -24,13 +25,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session({ secret: 'egg', resave: true, saveUninitialized: false }));
+app.use(session({ secret: "egg", resave: true, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 passportConfig();
 app.use("/", index);
 app.use("/users", users);
+app.use("/api/v1/", boards);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
