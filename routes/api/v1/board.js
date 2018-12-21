5e7debe52457497d86e0ca224001adba5abbc7c5
danny <pss>
2018-12-22 00:13:00 +0900

    board crud 추가하였습니다. service에

--- routes/api/v1/board.js
+++ routes/api/v1/board.js
@@ -7,5 +7,20 @@ router.get("/boards", async (req, res) => {
     res.send(await boardService.all());
 });
 
+router.get("/boards/:id", async (req, res) => {
+    res.send(await boardService.findByUser(req.params.id));
+});
+
+router.post("/boards", async (req, res) => {
+    res.send(await boardService.create(res.id));
+});
+
+router.put("/boards", async(req, res)=> {
+    res.send(await boardService.modify(res.id, res.contents));
+});
+
+router.delete("/boards", async(req, res)=> {
+    res.send(await boardService.delete(res.boardIdx));
+});
 
 module.exports = router;
