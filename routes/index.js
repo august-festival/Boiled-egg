5f6e3bf336738ef9395b81cd60c3d8916e2011ed
super-fishz <super_fishz@core-a.org>
2018-12-22 19:39:16 +0900

    static file 을 처리하기 위해 index router 수정

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,12 @@
 const express = require("express");
+const path = require("path");
 const model = require("../models/");
 console.log('router', process.env.NODE_ENV);
 
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", (req, res, next) => {
-    res.render("index", { title: "Boiled Egg" });
+router.get('/', function (req, res, next) {
+      res.sendFile(path.join(__dirname, '../public', 'index.html'))
 });
-router.get("/user/:id", (req, res, next) => {
-// if the user ID is 0, skip to the next route
-    if (req.params.id == 0) next("route");
-    // otherwise pass the control to the next middleware function in this stack
-    else next(); //
-}, (req, res, next) => {
-// render a regular page
-    res.render("regular");
-});
-
-// handler for the /user/:id path, which renders a special page
-router.get("/user/:id", (req, res, next) => {
-    res.render("special");
-});
-
 
 module.exports = router;
